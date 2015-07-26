dcad8f686adf301beb951fae14aa4b223daef1f3
creimers <christoph@superservice-international.com>
2015-07-26 19:24:39 +0200

    undo path changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 bootstraps a linux server in a very minimal way
+make sure you clone this into .bootstrap, or else the makefile won't work.
--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
 # VIMRC
 rm -f ~/.vimrc
-ln -s ~/.lnx-cms-bootstrap/.vimrc ~/.vimrc
+ln -s ~/.bootstrap/.vimrc ~/.vimrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall now
