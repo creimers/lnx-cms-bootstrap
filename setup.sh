1d3d2a8ea1da183ca16953e61aa80861bfd1cedd
creimers <christoph@superservice-international.com>
2015-07-26 19:18:05 +0200

    install vundle

--- setup.sh
+++ setup.sh
@@ -8,4 +8,5 @@ echo "ssh key copied to clipboard..."
 # VIMRC
 rm -f ~/.vimrc
 ln -s ~/.bootstrap/.vimrc ~/.vimrc
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall now
